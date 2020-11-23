97cd70bca30def28ff61b696eb72c26d2acc837d
Harshvivek Kashid <54810624+itzhv14@users.noreply.github.com>
2020-11-23 21:45:27 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-// // dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 // mongoose.connect("mongodb://localhost:27017/iotform", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
-// module.exports = {
-//     mongoURI: dbPassword
-// };
+module.exports = {
+    mongoURI: dbPassword
+};
